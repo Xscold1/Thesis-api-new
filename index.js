d9ffc8f8863ec33d4447fca15a6b8d0b128bfcfb
Xscold <103187013+Xscold@users.noreply.github.com>
2022-12-03 12:29:31 +0800

    cors

--- index.js
+++ index.js
@@ -37,13 +37,9 @@ app.use(function (req, res, next) {
 app.use(express.json());
 //app.use(cors());
 app.use(cookieParser());
-const corsOptions ={
-  origin:'*', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}
 
-app.use(cors(corsOptions)) // Use this after the variable declaration
+
+app.use(cors({origin: 'https://lobster-app-nmiq3.ondigitalocean.app/'})) // Use this after the variable declaration
 
 
 // To serve the public files like images, files, etc.
