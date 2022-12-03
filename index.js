1ef72d8a4dde1c06272fac66cd30081b233dc5e0
Xscold <103187013+Xscold@users.noreply.github.com>
2022-12-03 12:18:55 +0800

    cors

--- index.js
+++ index.js
@@ -16,8 +16,15 @@ app.use((req, res, next) => {
 
 //Middlewares
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
 app.use(cookieParser());
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 
 // To serve the public files like images, files, etc.
